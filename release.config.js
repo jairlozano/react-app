ddd54c5d4c6de6abab4d9edcdafe7387d17a428d
Jair Lozano <jair@hackmetrix.com>
2022-03-09 10:14:37 -0400

    fix(release): fix format of release config file

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/jairlozano/react-app",
-    plugins: [
-        "@semantic-release/commit-analyzer"
-        "@semantic-release/release-notes-generator"
-        "@semantic-release/github"
-    ]
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/jairlozano/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+};
